a5676e0c25b28405a52e5b9968e2a8d9ffdf61e1
Darkrulerz <dark_rulerz@hotmail.com>
2011-06-18 12:38:32 +0200

    Microsoft JDBC driver for SQLExpress 2008 R2 bijgevoegd en de connectie getest : werkt!
    Signed-off-by: Darkrulerz <dark_rulerz@hotmail.com>

--- src/connectivity/Dbmanager.java
+++ src/connectivity/Dbmanager.java
@@ -11,9 +11,9 @@ public class Dbmanager {
 
     public static final String JDBC_EXCEPTION = "JDBC Exception: ";
     public static final String SQL_EXCEPTION = "SQL Exception: ";
-    private static final String sqlexpress_port = "1433", sqlexpress_instance = "ZAANSMUSEUM", sqlexpress_address = "zm-server11.zaansmuseum.local", sqlexpress_user = "", sqlexpress_pass = "";
+    private static final String sqlexpress_instance = "ZAANSMUSEUM", sqlexpress_address = "zm-server11", sqlexpress_user = "sa";
     private static final String mysql_port = "3306", mysql_address = "127.0.0.1", mysql_user = "root", mysql_pass = "mangos";
-    private static final String databasename = "roosterprogramma";
+    private static final String databasename = "Roosterprogramma";
     public Connection connection;
     private static final boolean mysql = true;
 
@@ -31,10 +31,9 @@ public class Dbmanager {
                         + "?user=" + mysql_user
                         + "&password=" + mysql_pass;
             } else {
-                Class.forName("com.sqlserver.jdbc.SQLServerDriver");
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
                 url = "jdbc:sqlserver://" + sqlexpress_address
-                        + ":" + sqlexpress_port
-                        + ";instance=" + sqlexpress_instance
+                        + "\\" + sqlexpress_instance
                         + ";databaseName=" + databasename
                         + ";userName=" + sqlexpress_user
                         + ";password=" + sqlexpress_pass;
