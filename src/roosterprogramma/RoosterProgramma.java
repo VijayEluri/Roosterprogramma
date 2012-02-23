c15b1d3f02d0ba8c0728141ec758aa82709cc463
Darkrulerz <dark_rulerz@hotmail.com>
2012-02-23 10:21:56 +0100

    Sorteren van de employees
    Signed-off-by: Darkrulerz <dark_rulerz@hotmail.com>

--- src/roosterprogramma/RoosterProgramma.java
+++ src/roosterprogramma/RoosterProgramma.java
@@ -7,6 +7,8 @@ import java.awt.Toolkit;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -24,21 +26,37 @@ public class RoosterProgramma {
 
     private static RoosterProgramma instance = new RoosterProgramma();
     private static JFrame mainWindow;
+    /**
+     *
+     */
     public static final String NAME = "Rooster Programma";
     private Dbmanager dbManager;
     private QueryManager queryManager;
     private Settings settings;
     private Employee currentUser;
+    private JPanel prevPanel;
     private HashMap<Integer, Employee> employees = new HashMap<Integer, Employee>();
 
+    /**
+     *
+     * @return
+     */
     public static RoosterProgramma getInstance() {
         return instance;
     }
 
+    /**
+     *
+     * @return
+     */
     public static QueryManager getQueryManager() {
         return getInstance().queryManager;
     }
 
+    /**
+     *
+     * @param args
+     */
     public static void main(String args[]) {
         final RoosterProgramma applicatie = RoosterProgramma.getInstance();
         applicatie.startup();
@@ -53,6 +71,11 @@ public class RoosterProgramma {
         this.prevPanel = prevPanel;
         showPanel(panel);
     }
+
+    /**
+     *
+     * @param panel
+     */
     public void showPanel(JPanel panel) {
         mainWindow.getContentPane().removeAll();
         mainWindow.getContentPane().add(panel, BorderLayout.CENTER);
@@ -68,6 +91,9 @@ public class RoosterProgramma {
         return this.prevPanel;
     }
 
+    /**
+     *
+     */
     public void startup() {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -96,6 +122,9 @@ public class RoosterProgramma {
         mainWindow.setVisible(true);
     }
 
+    /**
+     *
+     */
     public void shutdown() {
         mainWindow.setVisible(false);
         mainWindow.dispose();
@@ -103,44 +132,82 @@ public class RoosterProgramma {
         System.exit(0);
     }
 
+    /**
+     *
+     * @return
+     */
     public Employee getCurrentUser() {
         return currentUser;
     }
 
+    /**
+     *
+     * @param employee
+     */
     public void setCurrentUser(Employee employee) {
         this.currentUser = employee;
     }
 
+    /**
+     *
+     * @return
+     */
     public Settings getSettings() {
         return settings;
     }
 
+    /**
+     *
+     * @param settings
+     */
     public void setSettings(Settings settings) {
         this.settings = settings;
     }
 
+    /**
+     *
+     */
     public void initializeEmployees() {
         for (Employee tmpEmployee : getQueryManager().getEmployees()) {
             employees.put(tmpEmployee.getEmployeeNumber(), tmpEmployee);
         }
     }
 
+    /**
+     *
+     */
     public void initializeSettings() {
         this.settings = getQueryManager().getSettings();
     }
 
+    /**
+     *
+     * @return
+     */
     public ArrayList<Employee> getEmployees() {
         ArrayList<Employee> employeeList = new ArrayList<Employee>();
         for (Employee tmpEmployee : employees.values()) {
             employeeList.add(tmpEmployee);
         }
-        return employeeList;
+        return sort(employeeList);
     }
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     public Employee getEmployee(int id) {
         return employees.get(id);
     }
 
+    /**
+     *
+     * @param firstname
+     * @param insertion
+     * @param familyname
+     * @return
+     */
     public Employee getEmployee(String firstname, String insertion, String familyname) {
         for (Employee tmpEmployee : employees.values()) {
             if (tmpEmployee.getFirstName().equalsIgnoreCase(firstname) && tmpEmployee.getFamilyName().equalsIgnoreCase(familyname)) {
@@ -152,6 +219,12 @@ public class RoosterProgramma {
         return null;
     }
 
+    /**
+     *
+     * @param firstname
+     * @param familyname
+     * @return
+     */
     public ArrayList<Employee> searchEmployee(String firstname, String familyname) {
         firstname = firstname.toLowerCase();
         familyname = familyname.toLowerCase();
@@ -172,19 +245,42 @@ public class RoosterProgramma {
                 return getEmployees();
             }
         }
-        return result;
+        return sort(result);
     }
 
+    /**
+     *
+     * @param employee
+     */
     public void removeEmployee(Employee employee) {
         employees.remove(employee.getEmployeeNumber());
     }
 
+    /**
+     *
+     * @param employee
+     */
     public void addEmployee(Employee employee) {
         employees.put(employee.getEmployeeNumber(), employee);
     }
 
+    /**
+     *
+     * @param employee
+     */
     public void changeEmployee(Employee employee) {
         removeEmployee(employee);
         addEmployee(employee);
+    }
+
+    public ArrayList<Employee> sort(ArrayList<Employee> employees) {
+        Collections.sort(employees, new Comparator<Employee>() {
+
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return o1.getEmployeeNumber() - o2.getEmployeeNumber();
+            }
+        });
+        return employees;
     }
 }
